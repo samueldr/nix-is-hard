e6ed209b38cf40c42c04fa8b0928821d4a1c398c
Samuel Dionne-Riel <samuel@dionne-riel.com>
2025-01-28 22:46:53 -0500

    buildProgram: rework with new mkElf semantics

--- default.nix
+++ default.nix
@@ -14,140 +14,70 @@ let
     toUint64
   ;
 
-  helloWorld =
+  helloElf =
     let
-      # TODO: build up an additional section instead of appending it to code section?
-      hello_world = ''
-        @ ${builtins.nixVersion}
-        @ ${builtins.currentSystem}
-        Etiam mauris nulla, suscipit ut gravida eget, tincidunt ac orci. Suspendisse 
-        aliquet lacus semper imperdiet elementum. Cras sodales lorem nec mauris 
-        bibendum elementum. Phasellus sed metus non orci posuere pretium feugiat at 
-        nisl. Suspendisse facilisis eget est non facilisis. Curabitur ac enim iaculis, 
-        vehicula purus ut, sodales ante. Nunc finibus imperdiet tellus in pharetra.
-      '';
-      hello_world_length = builtins.stringLength hello_world;
-      data = builtins.concatLists [
-        (basicLatinToBytes hello_world) [0]
-      ];
+      stdout = 1;
+      inherit (linux.x86_64.dsl) syscall;
     in
     buildProgram {
       code =
-        { load_addr, section_addr, ... }:
-        let
-          # FIXME: we're (still) slapping the data section right after the code section.
-          #        instead the sections should receive the different sections and their length+offsets.
-          #        then we'd put the string data into a section, and refer to that section
-          #        we would still need a sort of "string bag" to be able to find string offsets...
-          code_length = (builtins.length (code { }));
-          code =
-            { code_length ? 0 }:
-            let
-              inherit (linux.x86_64.dsl.syscall)
-                write
-                exit
-              ;
-              stdout = 1;
-            in
-            builtins.concatLists [
-              (write stdout (section_addr + code_length ) hello_world_length)
-              (exit 42)
-            ]
-          ;
-        in
-        code { inherit code_length; }
+        { getString, ... }:
+        builtins.concatLists [
+          (syscall.write stdout (getString "hello").addr (getString "hello").length)
+          (syscall.exit 0)
+        ]
       ;
-      inherit data;
+      strings =
+        {
+          hello = ''
+            @ ${builtins.nixVersion}
+            @ ${builtins.currentSystem}
+            Etiam mauris nulla, suscipit ut gravida eget, tincidunt ac orci. Suspendisse
+            aliquet lacus semper imperdiet elementum. Cras sodales lorem nec mauris
+            bibendum elementum. Phasellus sed metus non orci posuere pretium feugiat at
+            nisl. Suspendisse facilisis eget est non facilisis. Curabitur ac enim iaculis,
+            vehicula purus ut, sodales ante. Nunc finibus imperdiet tellus in pharetra.
+          '';
+        }
+      ;
     }
   ;
 
-  chmod =
+  chmodElf =
     let
-      path = "file.sh";
-      data = builtins.concatLists [
-        (basicLatinToBytes path)
-        [ 0 ]
-      ];
+      inherit (linux.x86_64.dsl) syscall;
     in
     buildProgram {
       code =
-        { load_addr, section_addr, ... }:
-
-        let
-          # TODO: use ARGV[1]?
-          code_length = (builtins.length (code { code_length = 0; }));
-          code =
-            { code_length }:
-            let
-              inherit (linux.x86_64.dsl.syscall)
-                chmod
-                exit
-              ;
-            in
-            builtins.concatLists [
-              (chmod (section_addr + code_length) 511 /* 0777 */)
-              # Move return value from previous syscall into ARG0
-              # Making the tool return the return value of the syscall
-              (arch.x86_64.instructions.MOV_reg "rdi" "rax")
-              (exit null)
-            ]
-          ;
-        in
-        code { inherit code_length; }
+        { getString, ... }:
+        builtins.concatLists [
+          (syscall.chmod (getString "path").addr 511 /* 0777 */)
+          # Move return value from previous syscall into ARG0
+          # Making the tool return the return value of the syscall
+          (arch.x86_64.instructions.MOV_reg "rdi" "rax")
+          (syscall.exit null)
+        ]
       ;
-      inherit data;
-    }
-  ;
-
-  newBin =
-    let
-      strings = lib.cstrings.mkCStrings {
-        hello = "Hello, World!\n";
-      };
-    in
-    # TODO: make a "strings aware" builder instead of using raw elf sections.
-    lib.mkElf {
-      sections = [
-        (lib.mkElfSection {
-          name = ".text";
-          type = "SHT_PROGBITS";
-          bytes =
-            let
-              stdout = 1;
-            in
-            { sections ? { ".rodata" = { addr = 0; }; }
-            , ...
-            }:
-            builtins.concatLists [
-              (linux.x86_64.dsl.syscall.write stdout (sections.".rodata".addr + strings.offsets.hello) (builtins.stringLength strings.strings.hello))
-              (linux.x86_64.dsl.syscall.exit 0)
-            ]
-          ;
-        })
-        (lib.mkElfSection {
-          name = ".rodata";
-          type = "SHT_PROGBITS";
-          bytes = strings.bytes;
-        })
-      ];
+      strings =
+        {
+          path = "file.sh";
+        }
+      ;
     }
   ;
 in
 {
   inherit lib;
+  inherit helloElf;
   hello = mkBinary {
     name = "main";
-    bytes = helloWorld;
+    bytes = helloElf.bytes;
     executable = true;
   };
+  inherit chmodElf;
   chmod = mkBinary {
     name = "main";
-    bytes = chmod;
-    executable = true;
-  };
-  newBin = mkBinary {
-    name = "main";
-    bytes = newBin.bytes;
+    bytes = chmodElf.bytes;
     executable = true;
   };
 }
--- lib/generic.nix
+++ lib/generic.nix
@@ -2,18 +2,70 @@
 
 let
   inherit (lib)
-    mkElf_legacy
+    mkElf
   ;
 in
 {
-  # Thin wrapper around mkElf.
+  #
+  # Wrapper to streamline building a program.
+  #
+  # The `code` argument takes the usual list of bytes, or function returning one.
+  # Using the DSL helpers to produce the list of bytes is expected.
+  # Data can be provided with either a list of bytes with `data`, or a `mkCStrings` input with `strings`.
+  #
+  # An helper function (`getString`) can be used to get the attrset for the string,
+  # augmented with the `addr` for the string, when `strings` is provided.
+  # It takes the attrname of the string as a parameter (or the full string, when using a list for `mkCStrings`).
+  #
   buildProgram =
     { code
-    , data
+    , data ? null
+    , strings ? null
     }:
 
-    mkElf_legacy rec {
-      inherit code data;
+    if data != null && strings != null
+    then throw "Calling buildProgram with both `data` and `strings` is invalid."
+    else
+    let data' = data; in # Break reference cycle
+    let strings' = strings; in # Break reference cycle
+    let
+      strings = if strings' != null then lib.cstrings.mkCStrings strings' else null;
+      data =
+        if !builtins.isNull strings
+        then strings.bytes
+        else data'
+      ;
+    in
+    mkElf {
+      sections = [
+        (lib.mkElfSection {
+          name = ".text";
+          type = "SHT_PROGBITS";
+          bytes =
+            if builtins.isFunction code && !builtins.isNull strings
+            then (args: code (args // {
+              getString =
+                name:
+                {
+                  addr = (args.sections.".rodata".addr or 0) + strings.offsets."${name}";
+                  offset = strings.offsets."${name}";
+                  length = builtins.stringLength strings.strings."${name}";
+                }
+              ;
+            }))
+            else code
+          ;
+        })
+      ]
+      ++
+      (lib.optional (data != null)
+        (lib.mkElfSection {
+          name = ".rodata";
+          type = "SHT_PROGBITS";
+          bytes = data;
+        })
+      )
+      ;
     }
   ;
 }
