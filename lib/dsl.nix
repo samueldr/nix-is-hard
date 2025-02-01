30ef9ddb5441d1108178fabf60aee97a7094d217
Samuel Dionne-Riel <samuel@dionne-riel.com>
2025-01-31 20:05:30 -0500

    dsl: Add bytesCount helper
    This will handle the comments...

--- lib/dsl.nix
+++ lib/dsl.nix
@@ -14,4 +14,9 @@
     builtins.filter
     (el: !builtins.isList el && !(el.__dsl or false) && el.__type or "" != "comment")
   ;
+
+  bytesCount =
+    bytes:
+    (builtins.length (lib.stripComments bytes))
+  ;
 }
