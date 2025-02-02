3e66e135474a771693a574fe249c58b9172be4d4
Samuel Dionne-Riel <samuel@dionne-riel.com>
2025-02-02 14:17:00 -0500

    tests: Test bytesUnsignedToNumber

--- tests/lib/binary.nix
+++ tests/lib/binary.nix
@@ -10,6 +10,24 @@
       [ 72 101 108 108 111 ]
     ;
   };
+  bytesUnsignedToNumber = {
+    i_0 = expect "bytesUnsignedToNumber to produce 0 for [ 0 ]"
+      (lib.bytesUnsignedToNumber [ 0 ])
+      0
+    ;
+    i_255 = expect "bytesUnsignedToNumber to produce 255 for [ 255 ]"
+      (lib.bytesUnsignedToNumber [ 255 ])
+      255
+    ;
+    i_256 = expect "bytesUnsignedToNumber to produce 256 for [ 0 1 ]"
+      (lib.bytesUnsignedToNumber [ 0 1 ])
+      256
+    ;
+    i_65535 = expect "bytesUnsignedToNumber to produce 65535 for [ 255 255 ]"
+      (lib.bytesUnsignedToNumber [ 255 255 ])
+      65535
+    ;
+  };
   numberToBytes = {
     singleByte = {
       i_0 = expect "numberToBytes to produce a single byte for (0)" (lib.numberToBytes 0) [ 0 ];
