d287fb392b4b75e00d5563a68be385f3fff94c2b
Samuel Dionne-Riel <samuel@dionne-riel.com>
2025-02-03 02:56:16 -0500

    arch/aarch64: Implement CMP_imm

--- lib/arch/aarch64.nix
+++ lib/arch/aarch64.nix
@@ -68,6 +68,50 @@ in
           [ (lib.comment "aarch64: syscall (svc 0)") 1 0 0 212 ]
         ;
 
+        CMP_imm =
+          #        ╒══════╤══════╤══════╤══════╤══════╤══════╤══════╤══════╦══════╤══════╤══════╤══════╤══════╤══════╤══════╤══════╕
+          # ╒══════╡  31  │  30  │  29  │  28  │  27  │  26  │  25  │  24  ║  23  │  22  │  21  │  20  │  19  │  18  │  17  │  16  │
+          # │  CMP │░░░░░░│   1  │   1  │   1  │   0  │   0  │   0  │   1  ║   0  │   0  │░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░│
+          # ╘══════╡  sf  │  op  │   S  │                                  ║      │  sh  │  imm12                              ... │
+          #        ├──────┬──────┬──────┬──────┬──────┬──────┬──────┬──────╥──────┬──────┬──────┬──────┬──────┬──────┬──────┬──────┤
+          # ╒══════╡  15  │  14  │  13  │  12  │  11  │  10  │   9  │   8  ║   7  │   6  │   5  │   4  │   3  │   2  │   1  │   0  │
+          # │  CMP │░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░│░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░│   1  │   1  │   1  │   1  │   1  │
+          # ╘══════╡ ... imm12                               │  Rn                              │  Rd                              │
+          #        ╘═══════════════════════════════════════════════════════════════════════════════════════════════════════════════╛
+          #         See: https://developer.arm.com/documentation/ddi0602/2024-12/Base-Instructions/CMP--immediate---Compare--immediate---an-alias-of-SUBS--immediate--
+          #         See: https://developer.arm.com/documentation/ddi0602/2024-12/Base-Instructions/SUBS--immediate---Subtract-immediate-value--setting-flags-
+          #         AKA: SUBS_64S_addsub_imm
+          #         AKA: SUBS_32S_addsub_imm
+          #         Encoded operation: 11111010010xxxxxxxxx10xxxxx0xxxx
+
+          reg: value:
+          let value' = value; in # break infrec
+          let
+            value =
+              if builtins.isInt value' then value' else
+              if builtins.isList value' then (lib.bytesUnsignedToNumber value') else
+              (throw "CMP_imm called with value of unexpected type (${builtins.typeOf value'}); expected integer or list.")
+            ;
+          in
+          let
+            reg' = registers."${reg}";
+            valueBytes = lib.ctypes.toUint64 value;
+            instruction =
+              builtins.foldl' builtins.add 0 ([]
+                ++ optional reg'.is64 (lib.bitShiftLeft 1 31)   # bit[31]    (size)
+                ++ [
+                    (lib.bitShiftLeft 226/* 0b1110_0010 */ 23)  # bit[23:30]
+                    (lib.bitShiftLeft value 10)                 # bit[10:21] (imm12)
+                    (lib.bitShiftLeft reg'.offset 5)            # bit[5:9]   (Rn)
+                    (31 /* 0b1_1111 */)                         # bit[0:4]   (Rd)
+                  ]
+              )
+            ;
+          in
+          [ (lib.comment "aarch64: CMP_imm ${reg} ${toString value}") ]
+          ++ (instructionToBytes instruction)
+        ;
+
         LDR_imm =
           #        ╒══════╤══════╤══════╤══════╤══════╤══════╤══════╤══════╦══════╤══════╤══════╤══════╤══════╤══════╤══════╤══════╕
           # ╒══════╡  31  │  30  │  29  │  28  │  27  │  26  │  25  │  24  ║  23  │  22  │  21  │  20  │  19  │  18  │  17  │  16  │
